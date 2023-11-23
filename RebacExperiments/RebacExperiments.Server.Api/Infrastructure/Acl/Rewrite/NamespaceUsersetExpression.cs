01d493ff7aa545e4834b65169dca39547acf087d
Philipp Wagner <pwag86@googlemail.com>
2023-11-23 10:04:04 +0100

    Move Data Model to Api Project for now

---
+++ RebacExperiments/RebacExperiments.Server.Api/Infrastructure/Acl/Rewrite/NamespaceUsersetExpression.cs
@@ -0,0 +1,25 @@
+﻿namespace RebacExperiments.Server.Api.Infrastructure.Acl.Rewrite
+{
+    /// <summary>
+    /// The root node of the Zanzibar Configuration language. It contains the 
+    /// name of the configured subject and an optional list of relations, expressed 
+    /// as <see cref="RelationUsersetExpression"/>.
+    /// </summary>
+    public class NamespaceUsersetExpression : UsersetExpression
+    {
+        /// <summary>
+        /// Gets or sets the Namespace being configured.
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Relations expressed by the Namespace configuration.
+        /// </summary>
+        public Dictionary<string, RelationUsersetExpression> Relations { get; set; } = new();
+
+        public override T Accept<T>(Visitor<T> visitor)
+        {
+            return visitor.VisitNamespaceUsersetExpr(this);
+        }
+    }
+}
